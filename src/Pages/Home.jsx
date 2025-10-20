63c19e889b6541ed1b2744a7667d4ad599c8495d
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-20 18:00:54 +0600

    added products all

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router";
 import ProductCard from "../Components/ProductCard";
+import useProducts from "../hooks/useProducts";
 
 const Home = () => {
-    const products = useLoaderData();
-    console.log(products);
+    const {products, loading, error} = useProducts()
     const featuredProducts = products.slice(0, 6)
 
   return (
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import useProducts from '../hooks/useProducts';
+import ProductCard from '../Components/ProductCard';
 
 const Products = () => {
+    const {products} = useProducts('')
     return (
-        <div>
-            <p>Products</p>
+       <div>
+        <div className="flex justify-between py-5 items-center">
+            <h1 className="text-3xl font-semibold">All Products</h1>
+            <button className="btn btn-outline">Search</button>
         </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product}></ProductCard>
+        ))}
+      </div>
+    </div>
     );
 };
 
